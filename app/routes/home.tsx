8a2235761da7178cb3a44dc2ff79915af852c35e
javimogan <diazalonsojavi@gmail.com>
2025-04-09 18:04:47 +0200

    Add ReactJS

---
+++ app/routes/home.tsx
@@ -0,0 +1,21 @@
+import type { Route } from "./+types/home";
+import { Welcome } from "../welcome/welcome";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Página de 1º ASIR" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+     return (
+      <div>
+        <h1 className="text-4xl">HOLA :)</h1>
+        <h2>Esto es un subtitulo</h2>
+      </div>
+     )
+}
+// export default function Home() {
+//   return <Welcome />;
+// }
