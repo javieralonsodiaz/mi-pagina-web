970b9a071ccf328d4db0754559be45e73dc6dbce
javimogan <diazalonsojavi@gmail.com>
2025-04-11 14:39:01 +0200

    Pagina en React-Router sin SSR

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,15 +3,15 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Página de 1º ASIR" },
+    { title: "1º ASIR" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
 export default function Home() {
      return (
-      <div>
-        <h1 className="text-4xl">HOLA 1º ASIR:)</h1>
+      <div className="flex flex-col border-4 border-pink-500 justify-center items-center h-screen">
+        <h1 className="text-4xl">HOLA 1º ASIR :)</h1>
         <h2>Esto es un subtitulo</h2>
       </div>
      )
